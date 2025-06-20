b460676a028878fb8a7b3f8c4fe77777128f3003
William Bell <epicugric@gmail.com>
2025-06-20 03:00:56 +0100

    work on hashmap

--- src/hashmap/hashmap.c
+++ src/hashmap/hashmap.c
@@ -4,9 +4,9 @@
 #include <stdlib.h>
 #include "../memory.h"
 
-struct table *createTable(int size)
+struct hashmap *createTable(int size)
 {
-    struct table *t = (struct table *)ar_alloc(sizeof(struct table));
+    struct hashmap *t = (struct hashmap *)ar_alloc(sizeof(struct hashmap));
     t->size = size;
     t->list = (struct node **)ar_alloc(sizeof(struct node *) * size);
     int i;
@@ -15,14 +15,39 @@ struct table *createTable(int size)
     return t;
 }
 
-int hashCode(struct table *t, int key)
+void resize_hashmap(struct hashmap *t)
+{
+    int old_size = t->size;
+    int new_size = old_size * 2;
+
+    struct node **old_list = t->list;
+
+    // Create new list
+    t->list = (struct node **)ar_alloc(sizeof(struct node *) * new_size);
+    for (int i = 0; i < new_size; i++)
+        t->list[i] = NULL;
+
+    t->size = new_size;
+    t->count = 0;
+
+    // Rehash old entries into new list
+    for (int i = 0; i < old_size; i++) {
+        struct node *temp = old_list[i];
+        while (temp) {
+            insert(t, temp->key, temp->val); // Will increment count
+            temp = temp->next;
+        }
+    }
+}
+
+int hashCode(struct hashmap *t, int key)
 {
     if (key < 0)
         return -(key % t->size);
     return key % t->size;
 }
 
-int remove(struct table *t, int key)
+int remove(struct hashmap *t, int key)
 {
     int pos = hashCode(t, key);
     struct node *list = t->list[pos];
@@ -46,12 +71,42 @@ int remove(struct table *t, int key)
     return 0;
 }
 
-void insert(struct table *t, int key, void* val)
+void resize(struct hashmap *t)
 {
+    int old_size = t->size;
+    int new_size = old_size * 2;
+
+    struct node **old_list = t->list;
+
+    // Create new list
+    t->list = (struct node **)ar_alloc(sizeof(struct node *) * new_size);
+    for (int i = 0; i < new_size; i++)
+        t->list[i] = NULL;
+
+    t->size = new_size;
+    t->count = 0;
+
+    // Rehash old entries into new list
+    for (int i = 0; i < old_size; i++) {
+        struct node *temp = old_list[i];
+        while (temp) {
+            insert(t, temp->key, temp->val); // Will increment count
+            temp = temp->next;
+        }
+    }
+}
+
+void insert(struct hashmap *t, int key, void* val)
+{
+    if ((t->count + 1) > t->size * 0.75) {
+        resize(t);
+    }
+
     int pos = hashCode(t, key);
     struct node *list = t->list[pos];
-    struct node *newNode = (struct node *)ar_alloc(sizeof(struct node));
     struct node *temp = list;
+
+    // Check if key exists → overwrite
     while (temp)
     {
         if (temp->key == key)
@@ -61,13 +116,17 @@ void insert(struct table *t, int key, void* val)
         }
         temp = temp->next;
     }
+
+    // Insert new node
+    struct node *newNode = (struct node *)ar_alloc(sizeof(struct node));
     newNode->key = key;
     newNode->val = val;
     newNode->next = list;
     t->list[pos] = newNode;
+    t->count++;
 }
 
-void *lookup(struct table *t, int key)
+void *lookup(struct hashmap *t, int key)
 {
     int pos = hashCode(t, key);
     struct node *list = t->list[pos];
---
+++ src/runtime/null/null.c
@@ -0,0 +1,5 @@
+#include "../runtime.h"
+
+void run_null(Translated *translated, RuntimeState *state) {
+    
+}
