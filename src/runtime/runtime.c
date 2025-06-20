7ef36ce0bc21750a28321911eeab964db668a844
William Bell <epicugric@gmail.com>
2025-06-20 02:50:05 +0100

    start working on runtime

---
+++ src/runtime/runtime.c
@@ -0,0 +1,23 @@
+#include "runtime.h"
+#include <stddef.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+uint64_t pop_bytecode(Translated *translated, RuntimeState *state) {
+  uint64_t *instruction = darray_get(&translated->bytecode, state->head++);
+  return *instruction;
+}
+
+void run_instruction(Translated *translated, RuntimeState *state) {
+  uint64_t opcode = pop_bytecode(translated, state);
+  switch (opcode) { case OP_LOAD_NULL: }
+}
+
+void runtime(Translated translated) {
+  RuntimeState state = {
+      checked_malloc(translated.registerCount * sizeof(size_t)), 0};
+    
+  while (state.head < translated.bytecode.size)
+    run_instruction(&translated, &state);
+  free(state.registers);
+}
--- src/translator/string/string.c
+++ src/translator/string/string.c
@@ -4,11 +4,11 @@
 #include <stdio.h>
 #include <string.h>
 
-size_t translate_parsed_string(Translated *translated, ParsedString parsedString, size_t to_register) {
+size_t translate_parsed_string(Translated *translated, ParsedString parsedString) {
   size_t string_pos = arena_push(&translated->constants, parsedString.string, parsedString.length);
-  set_registers(translated, to_register+1);
+  set_registers(translated, 1);
   size_t start = push_instruction_code(translated, OP_LOAD_CONST);
-  push_instruction_code(translated, to_register);
+  push_instruction_code(translated, 0);
   push_instruction_code(translated, TYPE_OP_STRING);
   push_instruction_code(translated,parsedString.length);
   push_instruction_code(translated, string_pos);
