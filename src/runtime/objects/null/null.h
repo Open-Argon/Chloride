49aef6d868bca4541aee22727085a7a095e2b564
William Bell <epicugric@gmail.com>
2025-06-24 03:49:05 +0100

    add null object and add load const

---
+++ src/runtime/objects/null/null.h
@@ -0,0 +1,10 @@
+#ifndef NULL_H
+#define NULL_H
+#include "../object.h"
+
+extern ArgonObject *ARGON_NULL;
+
+void init_null();
+
+
+#endif // NULL_H
--- src/runtime/objects/string/string.c
+++ src/runtime/objects/string/string.c
@@ -1,15 +1,22 @@
 #include "../object.h"
 #include <stdint.h>
+#include <stdio.h>
 
 ArgonObject *ARGON_STRING_TYPE = NULL;
+ArgonObject *ARGON_STRING_BASE = NULL;
 
 void init_string_type() {
   ARGON_STRING_TYPE = init_argon_object();
+
+  ARGON_STRING_BASE = init_argon_object();
+
 }
 
 ArgonObject *init_string_object(char*data, size_t length) {
+  printf("%s\n", data);
   ArgonObject * object = init_argon_object();
   object->typeObject = ARGON_STRING_TYPE;
+  object->baseObject = ARGON_STRING_BASE;
   object->value.as_str.data = data;
   object->value.as_str.length = length;
   return object;
