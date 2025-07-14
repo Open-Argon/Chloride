eb329504062d7ef2cee7c491923ff1b2f1cf61e7
Ugric <epicugric@gmail.com>
2025-07-14 05:46:54 +0100

    fix licensing

--- src/translator/dowrap/dowrap.c
+++ src/translator/dowrap/dowrap.c
@@ -1,3 +1,10 @@
+/*
+ * SPDX-FileCopyrightText: 2025 William Bell
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+
 #include "dowrap.h"
 #include <stddef.h>
 
--- src/translator/return/return.c
+++ src/translator/return/return.c
@@ -1,3 +1,9 @@
+/*
+ * SPDX-FileCopyrightText: 2025 William Bell
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
 #include "return.h"
 
 size_t translate_parsed_return(Translated *translated,
