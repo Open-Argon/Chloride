4b439fc52a7a73b0495eac46b486bce4cb92878e
William Bell <62452284+Ugric@users.noreply.github.com>
2025-08-14 05:42:38 +0100

    change function depending on the operation

--- src/translator/call/call.c
+++ src/translator/call/call.c
@@ -8,20 +8,26 @@
 size_t translate_parsed_call(Translated *translated, ParsedCall *call,
                              ArErr *err) {
   set_registers(translated, 1);
-  translate_parsed(translated, call->to_call, err);
-  size_t first = push_instruction_byte(translated, OP_INIT_CALL);
+  size_t first = translate_parsed(translated, call->to_call, err);
+  if (err->exists) {
+    return first;
+  }
+  push_instruction_byte(translated, OP_INIT_CALL);
   push_instruction_code(translated, call->args.size);
 
-  push_instruction_byte(translated, OP_NEW_SCOPE);
+  DArray *old_return_jumps = translated->return_jumps;
+  translated->return_jumps = NULL;
   for (size_t i = 0; i < call->args.size; i++) {
     translate_parsed(translated, darray_get(&call->args, i), err);
-    if (err->exists)
+    if (err->exists) {
+      translated->return_jumps = old_return_jumps;
       return first;
+    }
     push_instruction_byte(translated, OP_INSERT_ARG);
     push_instruction_code(translated, i);
   }
-  if (err->exists)
-    return first;
+
+  translated->return_jumps = old_return_jumps;
 
   push_instruction_byte(translated, OP_SOURCE_LOCATION);
   push_instruction_code(translated, call->line);
@@ -29,6 +35,5 @@ size_t translate_parsed_call(Translated *translated, ParsedCall *call,
   push_instruction_code(translated, 1);
 
   push_instruction_byte(translated, OP_CALL);
-  push_instruction_byte(translated, OP_POP_SCOPE);
   return first;
 }
