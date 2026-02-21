210b4e5a4a63180ee8b789b5efc7aa6501d2063f
Ugric <epicugric@gmail.com>
2026-02-21 23:15:13 +0000

    re enable small_object for better performance with smaller objects

--- src/runtime/objects/object.h
+++ src/runtime/objects/object.h
@@ -16,8 +16,10 @@ extern const char *built_in_field_names[BUILT_IN_FIELDS_COUNT];
 
 typedef struct ArgonObject ArgonObject;
 
+extern pthread_mutex_t objects_mutex;
+
 ArgonObject *new_class();
-// ArgonObject *new_small_instance(ArgonObject *of, size_t endSize);
+ArgonObject *new_small_instance(ArgonObject *of, size_t endSize);
 ArgonObject *new_instance(ArgonObject *of, size_t endSize);
 
 void init_built_in_field_hashes();
