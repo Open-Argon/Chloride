ef30ecbc39dc87f3661ab9721f64167fc79ea345
William Bell <epicugric@gmail.com>
2026-02-23 01:26:59 +0000

    remove dispatch.h on macos

--- src/RWLock.h
+++ src/RWLock.h
@@ -41,52 +41,52 @@ extern atomic_int thread_count;
 
 #define RWBLOCK
 
-#elif defined(__APPLE__)
-#include <dispatch/dispatch.h>
+// #elif defined(__APPLE__)
+// #include <dispatch/dispatch.h>
 
-typedef struct {
-  dispatch_queue_t queue;
-} RWLock;
+// typedef struct {
+//   dispatch_queue_t queue;
+// } RWLock;
 
-static inline void RWLOCK_CREATE(RWLock *lock) {
-  lock->queue =
-      dispatch_queue_create("rwlock.queue", DISPATCH_QUEUE_CONCURRENT);
-  if (!lock->queue)
-    abort();
-}
+// static inline void RWLOCK_CREATE(RWLock *lock) {
+//   lock->queue =
+//       dispatch_queue_create("rwlock.queue", DISPATCH_QUEUE_CONCURRENT);
+//   if (!lock->queue)
+//     abort();
+// }
 
-static inline void RWLOCK_DESTROY(RWLock *lock) {
-  (void)lock; // safe no-op
-}
+// static inline void RWLOCK_DESTROY(RWLock *lock) {
+//   (void)lock; // safe no-op
+// }
 
-// Assume thread_count is defined externally
-extern atomic_int thread_count;
+// // Assume thread_count is defined externally
+// extern atomic_int thread_count;
 
-// Execute a block inside a read lock (dispatch_sync allows parallel readers)
-#define RWLOCK_RDLOCK(l, block)                                                \
-  do {                                                                         \
-    if (atomic_load(&thread_count) == 0) {                                     \
-      block;                                                                   \
-    } else {                                                                   \
-      dispatch_sync((l).queue, ^{                                              \
-        block;                                                                 \
-      });                                                                      \
-    }                                                                          \
-  } while (0)
+// // Execute a block inside a read lock (dispatch_sync allows parallel readers)
+// #define RWLOCK_RDLOCK(l, block)                                                \
+//   do {                                                                         \
+//     if (atomic_load(&thread_count) == 0) {                                     \
+//       block;                                                                   \
+//     } else {                                                                   \
+//       dispatch_sync((l).queue, ^{                                              \
+//         block;                                                                 \
+//       });                                                                      \
+//     }                                                                          \
+//   } while (0)
 
-// Execute a block inside a write lock (barrier ensures exclusivity)
-#define RWLOCK_WRLOCK(l, block)                                                \
-  do {                                                                         \
-    if (atomic_load(&thread_count) == 0) {                                     \
-      block;                                                                   \
-    } else {                                                                   \
-      dispatch_barrier_sync((l).queue, ^{                                      \
-        block;                                                                 \
-      });                                                                      \
-    }                                                                          \
-  } while (0)
+// // Execute a block inside a write lock (barrier ensures exclusivity)
+// #define RWLOCK_WRLOCK(l, block)                                                \
+//   do {                                                                         \
+//     if (atomic_load(&thread_count) == 0) {                                     \
+//       block;                                                                   \
+//     } else {                                                                   \
+//       dispatch_barrier_sync((l).queue, ^{                                      \
+//         block;                                                                 \
+//       });                                                                      \
+//     }                                                                          \
+//   } while (0)
 
-#define RWBLOCK __block
+// #define RWBLOCK __block
 
 #else
 #include <pthread.h>
