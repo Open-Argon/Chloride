ffb26a276842220e35fb865af00a279c914ba8b2
Ugric <epicugric@gmail.com>
2026-03-24 23:52:34 +0000

    start working on trycatch system

---
+++ src/parser/trycatch/trycatch.c
@@ -0,0 +1,8 @@
+/*
+ * SPDX-FileCopyrightText: 2025 William Bell
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+#include "trycatch.h"
+#include "../../memory.h"
---
+++ src/parser/trycatch/trycatch.h
@@ -0,0 +1,11 @@
+/*
+ * SPDX-FileCopyrightText: 2025 William Bell
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+#ifndef TRYCATCH_H
+#define TRYCATCH_H
+
+
+#endif // TRYCATCH_H
