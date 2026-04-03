ca072ce925305f906cd062439926fa84fee5a662
Ugric <epicugric@gmail.com>
2026-04-03 23:09:06 +0100

    start working on try logic

---
+++ src/translator/try/try.c
@@ -0,0 +1,18 @@
+/*
+ * SPDX-FileCopyrightText: 2025 William Bell
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+#include "try.h"
+#include <stddef.h>
+#include "../translator.h"
+
+size_t translate_parsed_try(Translated *translated, ParsedTry *parsedTry,
+                              ArErr *err) {
+  set_registers(translated, 1);
+
+  size_t first;
+
+  return first;
+}
