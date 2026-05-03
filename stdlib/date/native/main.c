1788b2f0c995727fc679d91edb91f7a0ee2e1c0f
William Bell <william@wbell.dev>
2026-05-03 12:52:26 +0100

    make a more robust date system

--- stdlib/date/native/main.c
+++ stdlib/date/native/main.c
@@ -1,4 +1,4 @@
-// SPDX-FileCopyrightText: 2025 William Bell
+// SPDX-FileCopyrightText: 2026 William Bell
 //
 // SPDX-License-Identifier: LGPL-3.0-or-later
 
@@ -12,6 +12,12 @@
 #include <time.h>
 #include <unistd.h>
 
+static time_t portable_timegm(struct tm *t) {
+    tzset();
+    time_t local = mktime(t);
+    return local - timezone; // timezone is seconds west of UTC
+}
+
 ARGON_FUNCTION(time_now, {
   if (api->fix_to_arg_size(0, argc, err))
     return api->ARGON_NULL;
@@ -19,9 +25,7 @@ ARGON_FUNCTION(time_now, {
   struct timespec *ts = api->argon_buffer_to_buffer(ts_obj, err).data;
   if (api->is_error(err))
     return api->ARGON_NULL;
-
   clock_gettime(CLOCK_REALTIME, ts);
-
   return ts_obj;
 })
 
@@ -43,24 +47,31 @@ ARGON_FUNCTION(time_get_subsecond, {
   return api->i64_to_argon(ts->tv_nsec);
 })
 
-ARGON_FUNCTION(time_to_parts_local, {
+ARGON_FUNCTION(time_monotonic, {
+  if (api->fix_to_arg_size(0, argc, err))
+    return api->ARGON_NULL;
+  struct timespec ts;
+  clock_gettime(CLOCK_MONOTONIC, &ts);
+  return api->i64_to_argon((int64_t)ts.tv_sec * 1000000000 + ts.tv_nsec);
+})
+
+ARGON_FUNCTION(time_to_parts_utc, {
   int64_t seconds = api->argon_to_i64(argv[0], err);
   if (api->is_error(err))
     return api->ARGON_NULL;
   time_t t = (time_t)seconds;
-  struct tm *tm = localtime(&t);
+  struct tm *tm = gmtime(&t);
 
   ArgonObject *parts = api->create_argon_buffer(sizeof(struct tm));
   struct tm *buf = api->argon_buffer_to_buffer(parts, err).data;
   if (api->is_error(err))
     return api->ARGON_NULL;
-
   *buf = *tm;
   return parts;
 })
 
 #define GET_tm                                                                 \
-  if (api->fix_to_arg_size(1, argc, err))                                      \
+  if (api->fix_to_arg_size(1, argc, err))                                     \
     return api->ARGON_NULL;                                                    \
   struct tm *tm = api->argon_buffer_to_buffer(argv[0], err).data;
 
@@ -76,57 +87,70 @@ ARGON_FUNCTION(time_parts_get_minute,
                { GET_tm return api->i64_to_argon(tm->tm_min); })
 ARGON_FUNCTION(time_parts_get_second,
                { GET_tm return api->i64_to_argon(tm->tm_sec); })
+ARGON_FUNCTION(time_parts_get_wday,
+               { GET_tm return api->i64_to_argon(tm->tm_wday); }) // 0=Sunday
+ARGON_FUNCTION(time_parts_get_yday,
+               { GET_tm return api->i64_to_argon(tm->tm_yday + 1); }) // 1-365
 
 ARGON_FUNCTION(time_from_parts, {
   struct tm t = {0};
   t.tm_year = (int)api->argon_to_i64(argv[0], err) - 1900;
-  t.tm_mon = (int)api->argon_to_i64(argv[1], err) - 1;
+  t.tm_mon  = (int)api->argon_to_i64(argv[1], err) - 1;
   t.tm_mday = (int)api->argon_to_i64(argv[2], err);
   t.tm_hour = (int)api->argon_to_i64(argv[3], err);
-  t.tm_min = (int)api->argon_to_i64(argv[4], err);
-  t.tm_sec = (int)api->argon_to_i64(argv[5], err);
-  t.tm_isdst = -1; // let the OS figure out DST
+  t.tm_min  = (int)api->argon_to_i64(argv[4], err);
+  t.tm_sec  = (int)api->argon_to_i64(argv[5], err);
+  t.tm_isdst = -1;
   return api->i64_to_argon((int64_t)mktime(&t));
 })
 
-ARGON_FUNCTION(time_format, {
-  // argv[0] = parts buffer, argv[1] = format string
+ARGON_FUNCTION(time_from_parts_utc, {
   struct tm t = {0};
   t.tm_year = (int)api->argon_to_i64(argv[0], err) - 1900;
-  t.tm_mon = (int)api->argon_to_i64(argv[1], err) - 1;
+  t.tm_mon  = (int)api->argon_to_i64(argv[1], err) - 1;
   t.tm_mday = (int)api->argon_to_i64(argv[2], err);
   t.tm_hour = (int)api->argon_to_i64(argv[3], err);
-  t.tm_min = (int)api->argon_to_i64(argv[4], err);
-  t.tm_sec = (int)api->argon_to_i64(argv[5], err);
-  struct string fmt_str = api->argon_to_string(argv[6], err);
+  t.tm_min  = (int)api->argon_to_i64(argv[4], err);
+  t.tm_sec  = (int)api->argon_to_i64(argv[5], err);
+  t.tm_isdst = 0;
+  return api->i64_to_argon((int64_t)portable_timegm(&t));
+})
 
+ARGON_FUNCTION(time_format_utc, {
+  int64_t seconds = api->argon_to_i64(argv[0], err);
+  if (api->is_error(err))
+    return api->ARGON_NULL;
+  time_t t = (time_t)seconds;
+  struct tm *tm = gmtime(&t);
+
+  struct string fmt_str = api->argon_to_string(argv[1], err);
   char *fmt = malloc(fmt_str.length + 1);
-
   if (!fmt)
     return api->ARGON_NULL;
-
   memcpy(fmt, fmt_str.data, fmt_str.length);
   fmt[fmt_str.length] = '\0';
 
   char buf[256];
-  size_t length = strftime(buf, sizeof(buf), fmt, &t);
+  size_t length = strftime(buf, sizeof(buf), fmt, tm);
   free(fmt);
   return api->string_to_argon((struct string){buf, length});
 })
 
 ARGON_FUNCTION(time_parse, {
   struct tm t = {0};
-  struct string str_str = api->argon_to_string(argv[0], err);  // was argv[6]
+  struct string str_str = api->argon_to_string(argv[0], err);
   char *str = malloc(str_str.length + 1);
-  if (!str)                                                      // was if (str)
+  if (!str)
     return api->ARGON_NULL;
   memcpy(str, str_str.data, str_str.length);
   str[str_str.length] = '\0';
 
-  struct string fmt_str = api->argon_to_string(argv[1], err);  // was argv[6]
+  struct string fmt_str = api->argon_to_string(argv[1], err);
   char *fmt = malloc(fmt_str.length + 1);
-  if (!fmt)                                                      // was if (fmt)
+  if (!fmt) {
+    free(str);
     return api->ARGON_NULL;
+  }
   memcpy(fmt, fmt_str.data, fmt_str.length);
   fmt[fmt_str.length] = '\0';
 
@@ -142,22 +166,6 @@ ARGON_FUNCTION(time_parse, {
   return parts;
 })
 
-ARGON_FUNCTION(time_to_parts_utc, {
-  int64_t seconds = api->argon_to_i64(argv[0], err);
-  if (api->is_error(err))
-    return api->ARGON_NULL;
-  time_t t = (time_t)seconds;
-  struct tm *tm = gmtime(&t);
-
-  ArgonObject *parts = api->create_argon_buffer(sizeof(struct tm));
-  struct tm *buf = api->argon_buffer_to_buffer(parts, err).data;
-  if (api->is_error(err))
-    return api->ARGON_NULL;
-
-  *buf = *tm;
-  return parts;
-})
-
 void argon_module_init(ArgonState *vm, ArgonNativeAPI *api, ArgonError *err,
                        ArgonObjectRegister *reg) {
   (void)vm;
@@ -165,15 +173,18 @@ void argon_module_init(ArgonState *vm, ArgonNativeAPI *api, ArgonError *err,
   REGISTER_ARGON_FUNCTION(time_now)
   REGISTER_ARGON_FUNCTION(time_get_seconds)
   REGISTER_ARGON_FUNCTION(time_get_subsecond)
-  REGISTER_ARGON_FUNCTION(time_to_parts_local)
+  REGISTER_ARGON_FUNCTION(time_monotonic)
+  REGISTER_ARGON_FUNCTION(time_to_parts_utc)
   REGISTER_ARGON_FUNCTION(time_parts_get_year)
   REGISTER_ARGON_FUNCTION(time_parts_get_month)
   REGISTER_ARGON_FUNCTION(time_parts_get_day)
   REGISTER_ARGON_FUNCTION(time_parts_get_hour)
   REGISTER_ARGON_FUNCTION(time_parts_get_minute)
   REGISTER_ARGON_FUNCTION(time_parts_get_second)
+  REGISTER_ARGON_FUNCTION(time_parts_get_wday)
+  REGISTER_ARGON_FUNCTION(time_parts_get_yday)
   REGISTER_ARGON_FUNCTION(time_from_parts)
-  REGISTER_ARGON_FUNCTION(time_format)
+  REGISTER_ARGON_FUNCTION(time_from_parts_utc)
+  REGISTER_ARGON_FUNCTION(time_format_utc)
   REGISTER_ARGON_FUNCTION(time_parse)
-  REGISTER_ARGON_FUNCTION(time_to_parts_utc)
 }
