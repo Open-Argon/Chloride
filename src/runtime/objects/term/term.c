1e9889c7fc36b35bf9c1d86e7b1d4027c1c3d672
William Bell <william@wbell.dev>
2026-05-03 12:10:13 +0100

    fix seg fault and memory leak in term.input

--- src/runtime/objects/term/term.c
+++ src/runtime/objects/term/term.c
@@ -7,8 +7,10 @@
 #include "../../../shell.h"
 #include "../../call/call.h"
 #include "../../objects/literals/literals.h"
+#include <inttypes.h>
 #include <stddef.h>
 #include <stdio.h>
+#include <string.h>
 
 ARGON_METHOD(term, log, {
   (void)api;
@@ -32,25 +34,38 @@ ARGON_METHOD(term, log, {
 })
 
 ARGON_METHOD(term, input, {
-  (void)api;
-  for (size_t i = 0; i < argc; i++) {
-    if (i != 0)
-      printf(" ");
+  if (argc >= 2)
+    api->throw_argon_error(err, api->RuntimeError,
+                           "expects at most 1 argument, got %" PRIu64, argc);
+  struct string prompt_str = {NULL, 0};
+  if (argc == 1) {
     ArgonObject *string_convert_method = get_builtin_field_for_class(
-        get_builtin_field(argv[i], __class__), __string__, argv[i]);
+        get_builtin_field(argv[0], __class__), __string__, argv[0]);
 
     if (string_convert_method) {
       ArgonObject *string_object =
           argon_call(string_convert_method, 0, NULL, NULL, err, state);
-      struct string string = api->argon_to_string(string_object, err);
+      prompt_str = api->argon_to_string(string_object, err);
       if (api->is_error(err))
         return ARGON_NULL;
-      fwrite(string.data, sizeof(char), string.length, stdout);
     }
   }
-  fflush(stdout);
 
-  char *user_input = input("");
-  
-  return api->string_to_argon((struct string){user_input, strlen(user_input)});
+  char *prompt = malloc(prompt_str.length + 1);
+  if (!prompt)
+    return api->throw_argon_error(err, api->RuntimeError, "out of memory");
+
+  memcpy(prompt, prompt_str.data, prompt_str.length);
+  prompt[prompt_str.length] = '\0';
+
+  char *user_input = input(prompt);
+  free(prompt);
+  if (!user_input)
+    return api->string_to_argon((struct string){NULL, 0});
+
+  ArgonObject *result =
+      api->string_to_argon((struct string){user_input, strlen(user_input)});
+  free(user_input);
+
+  return result;
 })
