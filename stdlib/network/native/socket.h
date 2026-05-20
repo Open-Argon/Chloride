766a51f91a8e6f2fc13e32f6ccba92f2c5508759
William Bell <william@wbell.dev>
2026-05-20 14:26:25 +0100

    fix licensing issues

--- stdlib/network/native/socket.h
+++ stdlib/network/native/socket.h
@@ -1,8 +1,4 @@
-/*
- * SPDX-FileCopyrightText: 2026 William Bell
- */
-
-// SPDX-FileCopyrightText: 2025 William Bell
+// SPDX-FileCopyrightText: 2025, 2026 William Bell
 //
 // SPDX-License-Identifier: LGPL-3.0-or-later
 
